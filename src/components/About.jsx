a8b60a417f765befac1af762a1d6a7afb84c9117
Sivarakshan <sivarakshan60@gmail.com>
2026-02-19 19:29:40 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import about from "../assets/my image.png";
+import about from "../assets/about.png";
 import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 
@@ -112,3 +112,4 @@ const About = () => {
 };
 
 export default About;
+
